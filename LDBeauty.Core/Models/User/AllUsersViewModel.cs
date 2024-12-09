9bf13c08f5379a3b1f94b4a1de24c06f02d8e1c8
marto05 <marto.05@abv.bg>
2024-12-09 22:48:59 +0200

    Tenth

---
+++ LDBeauty.Core/Models/User/AllUsersViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LDBeauty.Core.Models.User
+{
+    public class AllUsersViewModel
+    {
+        public string Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Email { get; set; }
+
+        public string Phone { get; set; }
+
+    }
+}
---
+++ LDBeauty.Core/Models/User/UserOrderViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LDBeauty.Core.Models.User
+{
+    public class UserOrderViewModel
+    {
+        public string Id { get; set; }
+
+        public int CartId { get; set; }
+
+        [Required]
+        [StringLength(50, ErrorMessage = "The {0} must be at least {2} and max {1} characters long.", MinimumLength = 3)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [StringLength(50, ErrorMessage = "The {0} must be at least {2} and max {1} characters long.", MinimumLength = 3)]
+        public string LastName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(200, ErrorMessage = "The {0} must be at least {2} and max {1} characters long.", MinimumLength = 10)]
+        public string Address { get; set; }
+
+        [Required]
+        [RegularExpression(@"^\d{10}$", ErrorMessage = "The phone number must be 10 digits long!")]
+        public string Phone { get; set; }
+    }
+}
